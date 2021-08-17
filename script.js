a41d3c66617c48aad607a1dd18d071d3d0660676
laziz007007 <wmfederal0777@gmail.com>
2021-08-17 17:37:15 +0500

    try

---
+++ script.js
@@ -0,0 +1,45 @@
+let inp = document.querySelector('input')
+let btn = document.querySelector('button')
+let box = document.querySelector('.box')
+
+
+
+
+let obj=[
+    {name:"Aziz",surname:"Ergashev",age:19,job:"bloger",city:"Kumkurgan"},
+    {name:"Laziz",surname:"Jurayev",age:20,job:"teacher",city:"Tashkent"},
+    {name:"Muxlisa",surname:"Abduraxmonova",age:26,job:"teacher",city:"Denov"},
+    {name:"Alisher",surname:"Boboxonov",age:21,job:"no job",city:"Angor"},
+    {name:"Munisa",surname:"Azizova",age:16,job:"cooking",city:"Shurchi"}
+]
+
+let photos = [
+    'https://wallpapercave.com/wp/wp3867881.jpg',
+    'https://cdn.pixabay.com/photo/2019/11/03/23/27/black-businessman-4599846_1280.jpg',
+    'https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YnVzaW5lc3MlMjB3b21hbnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
+    'https://artriva.com/media/k2/items/cache/c889234799e865bbe90cee71f6cd2e53_XL.jpg',
+    'https://res.cloudinary.com/twenty20/private_images/t_watermark-criss-cross-10/v1603756572000/photosp/dc294465-328b-484a-a1d7-60cd24a3e52d/stock-photo-young-woman-woman-professional-educated-business-woman-business-professional-dc294465-328b-484a-a1d7-60cd24a3e52d.jpg'
+    
+]
+
+let name = [
+    'Aziz ',
+    'Laziz ',
+    'Muxlisa ',
+    'Alisher ',
+    'Munisa '
+]
+let surname = [
+    'Ergashev',
+    'Jurayev',
+    'Abduraxmonova',
+    'Boboxonov',
+    'Azizova'
+]
+let arr = []
+btn.addEventListener('click',() =>{
+    
+})
+
+
+
